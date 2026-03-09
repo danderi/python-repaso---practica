933a52383b131d3a4944f0c122fccf08c904e6c9
Daniel Anderi <100786824+danderi@users.noreply.github.com>
2026-03-09 00:31:43 +0000

    ejercicio 9 done

--- exercises/09-Random-Numbers/app.py
+++ exercises/09-Random-Numbers/app.py
@@ -1,8 +1,8 @@
-import random
+from random import randint
 
 def get_randomInt():
   	# ✅ ↓ CHANGE ONLY THIS ONE LINE BELOW ↓ ✅
-	random_number = random.random()
+	random_number = randint(1, 10)
 	return random_number
 
 print(get_randomInt())
