a754c53a8757eea2954ebbc01f504d3d15249418
Daniel Anderi <100786824+danderi@users.noreply.github.com>
2026-03-24 18:47:47 +0000

    done until exercise 16

--- exercises/14-Create-A-For-Loop/app.py
+++ exercises/14-Create-A-For-Loop/app.py
@@ -1,5 +1,10 @@
 def standards_maker():
     # ✅↓ Write your code here ↓✅
-
-
+    count=0
+    for i in range(300):
+        #print("Haré preguntas si estoy atascado")
+        count+=1
+    return count
 # ✅↓ remember to call the function outside (here) ↓✅
+
+print(standards_maker())
--- exercises/16-Random-Colors-Loop/app.py
+++ exercises/16-Random-Colors-Loop/app.py
@@ -1,4 +1,4 @@
-import random
+from random import randint
 
 def get_color(color_number=4):
     # Making sure is a number and not a string
@@ -20,6 +20,7 @@ def get_allStudentColors():
     example_color = get_color(1)
     students_array = []
     # ✅ ↓ your loop here ↓ ✅
-    
+    #for i in range(10):
+    return [get_color(randint(0,3)) for i in range(10)]
 
 print(get_allStudentColors())
